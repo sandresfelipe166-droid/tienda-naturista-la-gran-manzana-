d3a7b3f47b02d344af5972a90f66cb3391760c49
felipe2223223 <andres-segura@upc.edu.co>
2025-09-30 19:26:38 -0500

    Normalize line endings to LF and fix alembic migration files

--- alembic/versions/2c99e3aaece6_merge_heads.py
+++ alembic/versions/2c99e3aaece6_merge_heads.py
@@ -12,17 +12,17 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '2c99e3aaece6'
-down_revision: Union[str, Sequence[str], None] = ('12453312962c', '999999999997')
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "2c99e3aaece6"
+down_revision: Union[str, Sequence[str], None] = ("12453312962c", "999999999997")
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
+    """Merge revision: normalmente no operaciones, solo marca unión de heads."""
     pass
 
 
 def downgrade() -> None:
-    """Downgrade schema."""
+    """En un merge head normalmente no hay downgrade automático."""
     pass
